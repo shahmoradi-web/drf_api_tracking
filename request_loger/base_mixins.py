2f89de2de633259e6edbf098536a3288b937d0e0
narges <narges.shahmoradi.01@gmail.com>
2025-10-29 18:51:10 +0330

    add should_log and handle_exeption Features

--- request_loger/base_mixins.py
+++ request_loger/base_mixins.py
@@ -1,10 +1,14 @@
 import ipaddress
+import traceback
 
 from django.utils.timezone import now
 from .app_settings import app_settings
 
 
 class BaseLogginMixin:
+    
+    logging_methods = "__all__"
+
 
     def initial(self, request, *args, **kwargs):
         self.log = {"requested_at": now()}
@@ -12,26 +16,33 @@ class BaseLogginMixin:
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super(BaseLogginMixin, self).finalize_response(request, response, *args, **kwargs)
-        user = self._get_user(request)
 
-        self.log.update(
-            {"remote_addr": self._get_ip_address(request),
-             "view": self._get_view_name(request),
-             "view_method": self._get_view_method(request),
-             "path": self._get_path(request),
-             "host": request.get_host(),
-             "method": request.method,
-             "user": user,
-             "username_persistent": user.get_username() if user else "Anonymous",
-             "response_ms": self._get_response_ms(),
-             "status_code": response.status_code,
-             }
-        )
-        self.handle_log()
+        if self.should_log(request, response):
+            user = self._get_user(request)
+            self.log.update(
+                {"remote_addr": self._get_ip_address(request),
+                "view": self._get_view_name(request),
+                "view_method": self._get_view_method(request),
+                "path": self._get_path(request),
+                "host": request.get_host(),
+                "method": request.method,
+                "user": user,
+                "username_persistent": user.get_username() if user else "Anonymous",
+                "response_ms": self._get_response_ms(),
+                "status_code": response.status_code,
+                }
+            )
+            self.handle_log()
         return response
 
     def handle_log(self):
         raise NotImplementedError
+
+    def handle_exception(self, exc):
+        response = super(BaseLogginMixin, self).handle_exception(exc)
+        self.log["errors"] = traceback.format_exc()
+
+        return response
 
 
     def _get_ip_address(self, request):
@@ -89,3 +100,8 @@ class BaseLogginMixin:
     def _get_path(self, request):
         """Get the request path and truncate it"""
         return request.path[: app_settings.PATH_LENGTH]
+
+    def should_log(self,request,response):
+        return(
+            self.logging_methods == '__all__' or request.method in self.logging_methods
+        )
--- request_loger/views.py
+++ request_loger/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+
+from request_loger.base_mixins import BaseLogginMixin
+
+
 # Create your views here.
 
 
-class Home(APIView):
+class Home(BaseLogginMixin,APIView):
+
+    logging_methods = ['GET', 'POST']
     def get(self, request):
         return Response({'message': 'Hello World!'})
