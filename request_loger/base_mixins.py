2d86ef22ab3cf8f3df0a43b3b15bad42b30c63f0
narges <narges.shahmoradi.01@gmail.com>
2025-10-30 13:25:26 +0330

    add sensitive_fields

--- request_loger/base_mixins.py
+++ request_loger/base_mixins.py
@@ -1,14 +1,18 @@
+import ast
 import ipaddress
+import logging
 import traceback
 
 from django.utils.timezone import now
 from .app_settings import app_settings
 
+logger = logging.getLogger(__name__)
 
 class BaseLogginMixin:
-    
-    logging_methods = "__all__"
 
+    logging_methods = "__all__"
+    CLEANED_SUBSTITUTE = "********************"
+    sensitive_fields = {}
 
     def initial(self, request, *args, **kwargs):
         self.log = {"requested_at": now()}
@@ -16,6 +20,13 @@ class BaseLogginMixin:
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super(BaseLogginMixin, self).finalize_response(request, response, *args, **kwargs)
+
+        if response.streaming:
+            rendered_content = None
+        elif hasattr(response, "rendered_content"):
+            rendered_content = response.rendered_content
+        else:
+            rendered_content = response.getvalue()
 
         if self.should_log(request, response):
             user = self._get_user(request)
@@ -30,9 +41,15 @@ class BaseLogginMixin:
                 "username_persistent": user.get_username() if user else "Anonymous",
                 "response_ms": self._get_response_ms(),
                 "status_code": response.status_code,
+                 "query_params": self._clean_data(request.query_params.dict()),
+                 "response": self._clean_data(rendered_content),
                 }
             )
-            self.handle_log()
+            try:
+                self.handle_log()
+            except Exception:
+                logger.exception("Logging API call raise exception!")
+
         return response
 
     def handle_log(self):
@@ -105,3 +122,34 @@ class BaseLogginMixin:
         return(
             self.logging_methods == '__all__' or request.method in self.logging_methods
         )
+
+    def _clean_data(self, data):
+
+        if isinstance(data, list):
+            return [self._clean_data(d) for d in data]
+
+        if isinstance(data, dict):
+
+            SENSITIVE_FIELDS = {
+                "api",
+                "token",
+                "key",
+                "secret",
+                "password",
+                "signature",
+            }
+
+            data = dict(data)
+            if self.sensitive_fields:
+                SENSITIVE_FIELDS = SENSITIVE_FIELDS | {field.lower() for field in self.sensitive_fields}
+
+            for key, value in data.items():
+                try:
+                    value = ast.literal_eval(value)
+                except (ValueError, SyntaxError):
+                    pass
+                if isinstance(value, (list, dict)):
+                    data[key] = self._clean_data(value)
+                if key.lower() in SENSITIVE_FIELDS:
+                    data[key] = self.CLEANED_SUBSTITUTE
+        return data
